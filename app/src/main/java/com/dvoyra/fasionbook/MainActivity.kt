0f7e96fc7650fcdfeb0ef9f51f966d47da469008
Dvoyra Kaller <dvoyrakaller@gmail.com>
2021-10-22 15:04:18 +0300

    1

--- app/src/main/java/com/dvoyra/fasionbook/MainActivity.kt
+++ app/src/main/java/com/dvoyra/fasionbook/MainActivity.kt
@@ -228,16 +228,15 @@ class MainActivity : Activity() {
 
     @Throws(IOException::class)
     private fun createImageFile(): File {
-        // Create an image file name
         val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss").format(Date())
         val imageFileName = "JPEG_" + timeStamp + "_"
         val storageDir = Environment.getExternalStoragePublicDirectory(
             Environment.DIRECTORY_PICTURES
         )
         return File.createTempFile(
-            imageFileName, /* prefix */
-            ".jpg", /* suffix */
-            storageDir      /* directory */
+            imageFileName,
+            ".jpg",
+            storageDir
         )
     }
 
